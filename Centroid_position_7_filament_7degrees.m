ad38d96c43fcb64a217040861d0915f94186abaf
Sakbé Corona <39100793+Sakbe@users.noreply.github.com>
2019-02-20 18:40:55 +0000

    2-20

--- Centroid_position_7_filament_7degrees.m
+++ Centroid_position_7_filament_7degrees.m
@@ -41,8 +41,8 @@ end
 %%%Experimental mesurements[Wb]
 
 Mirnv_10_fact=1.2803;
-time_ins=116;
-time_index=find(time == 116); %%% Select a time moment where there is plasma current! in [ms]
+time_ins=110;
+time_index=find(time == time_ins ); %%% Select a time moment where there is plasma current! in [ms]
 
 %%%%%%%%%% Find the exprimental values for that time moment
 
@@ -66,16 +66,23 @@ Mirnv_B_exp_corr=double(Mirnv_flux_corr/(50*49e-6)); %%%% [T]
 %%%%%% 6 sorrounding filaments - 1 degree of freedom (I)
 
 %%%%Lets put boundaries
-low_bnd=[0,0,0,0,0,0,0];
+% if Mirnv_B_exp_corr(1)>0
+% low_bnd=[-4000,-4000,-4000,-4000,-4000,-4000,-4000];
+% high_bnd=[0,0,0,0,0,0,0];
+% Ini_cond=[-1000,-500,-500,-500,-500,-500,-500];
+% else
+    low_bnd=[0,0,0,0,0,0,0];
 high_bnd=[4000,4000,4000,4000,4000,4000,4000];
+Ini_cond=[1000,500,500,500,500,500,500];
+% end
 
 A=-eye(7);
 b=zeros(7,1);
 
 % fval_multi=fminsearch(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp,x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),R_filaments,z_filaments,R_mirn,z_mirn),[0.5,46.5,500,500,500,500,500,500,500])
 
-fval_multi=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp,z_filaments(1),R_filaments(1),x(1),x(2),x(3),x(4),x(5),x(6),x(7),...
-    R_filaments,z_filaments,R_mirn,z_mirn),[300,1500,1500,500,500,500,500],[],[],[],[],low_bnd,high_bnd)
+% fval_multi=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp,z_filaments(1),R_filaments(1),x(1),x(2),x(3),x(4),x(5),x(6),x(7),...
+%     R_filaments,z_filaments,R_mirn,z_mirn),[300,1500,1500,500,500,500,500],[],[],[],[],low_bnd,high_bnd)
 
 % fval_multi=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp,x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),R_filaments,z_filaments,R_mirn,z_mirn),[0.5,46.5,1000,500,500,500,500,500,500],A,b)
 
@@ -85,39 +92,40 @@ fval_multi=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp,z_filaments(1),R_fil
 
 fval_multi_corr=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp_corr,z_filaments(1),R_filaments(1),x(1),...
     x(2),x(3),x(4),x(5),x(6),x(7),R_filaments,z_filaments,R_mirn,z_mirn),...
-    [300,1500,1500,500,500,500,500],[],[],[],[],low_bnd,high_bnd)
+    Ini_cond,[],[],[],[],low_bnd,high_bnd)
 
 % fval_multi_corr=fmincon(@(x) ErrorMirnFuncMultiFilam(Mirnv_B_exp_corr,x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),R_filaments,z_filaments,R_mirn,z_mirn),[0.5,46.5,1000,500,500,500,500,500,500],A,b)
  
-%%%%Lets check how close is our minimization values to the experimental
+%% Lets check how close is our minimization values to the experimental
 %%%%ones by applaying Biot-Savart with them 
 
-xx_multi=BmagnMultiModule(z_filaments(1),R_filaments(1),fval_multi,R_filaments,z_filaments,R_mirn,z_mirn);
+% %xx_multi=BmagnMultiModule(z_filaments(1),R_filaments(1),fval_multi,R_filaments,z_filaments,R_mirn,z_mirn);
+% fval_multi_corr =-1.0e+03 *[ 0.0005    1.5328    1.1538    0.3305    0.0001    0.0008    1.1140];
 
 xx_multi_corr=BmagnMultiModule(z_filaments(1),R_filaments(1),fval_multi_corr,R_filaments,z_filaments,R_mirn,z_mirn);
 
 
 %%%% Error
 
-RMSE_multi=sqrt(mean((xx_multi(:)-Mirnv_B_exp(:)))^2);
+% RMSE_multi=sqrt(mean((xx_multi(:)-Mirnv_B_exp(:)))^2);
 RMSE_multi_corr=sqrt(mean((xx_multi_corr(:)-Mirnv_B_exp_corr(:)))^2);
-Error_multi=sum(abs(xx_multi-Mirnv_B_exp))/12;
+% Error_multi=sum(abs(xx_multi-Mirnv_B_exp))/12;
 Error_multi_corr=sum(abs(xx_multi_corr-Mirnv_B_exp_corr))/12;
 
 %%%%%%%%%%Plotting
 %%%%%%Multifilament plots
 
 
-figure(8)
-plot([1,2,3,4,5,6,7,8,9,10,11,12],1000*Mirnv_B_exp,'-o')
-hold on
-plot([1,2,3,4,5,6,7,8,9,10,11,12],1000*xx_multi,'-*')
-grid on
-title('Shot #45410  t=195[ms]  Ip~4.1[kA] (Multifilament)')
-legend('Experimental Data ','Biot-savart  (optimized )')
-xlabel('Mirnov #')
-ylabel('Optimization [mT]')
-axis equal
+% figure(8)
+% plot([1,2,3,4,5,6,7,8,9,10,11,12],1000*Mirnv_B_exp,'-o')
+% hold on
+% plot([1,2,3,4,5,6,7,8,9,10,11,12],1000*xx_multi,'-*')
+% grid on
+% title('Shot #45410  t=195[ms]  Ip~4.1[kA] (Multifilament)')
+% legend('Experimental Data ','Biot-savart  (optimized )')
+% xlabel('Mirnov #')
+% ylabel('Optimization [mT]')
+% axis equal
 
 figure(9)
 plot([1,2,3,4,5,6,7,8,9,10,11,12],1000*Mirnv_B_exp_corr,'-o')
