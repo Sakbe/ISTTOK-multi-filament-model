d1e6f8d126984d1c779e736e67a67af4fda674e4
Sakbe <sacbecorona@hotmail.com>
2019-02-27 20:35:51 +0000

    27-02

--- ErrorMirnFuncMultiFilam.m
+++ ErrorMirnFuncMultiFilam.m
@@ -3,9 +3,11 @@ function error=ErrorMirnFuncMultiFilam(Mirnv_B_exp,Z_filament,R_filament,I_filam
 
 
 for i=1:12
-vector(i,[1,2])=[z_filaments(1)-z_mirnv(i),R_filaments(1)-r_mirnv(i)];%Vector from center of chamber to mirnov center
-unit_vec(i,[1,2])=[vector(i,[1,2])]./norm(vector(i,[1,2])); %% Unit vector
-norm_vec(i,[1,2])=[unit_vec(i,2),-unit_vec(i,1)];%%%  Normal vector, coil direction
+    for j=1:7
+vector(i,j,[1,2])=[z_filaments(j)-z_mirnv(i),R_filaments(j)-r_mirnv(i)];%Vector from center of chamber to mirnov center
+unit_vector(i,j,:)=[vector(i,j,1),vector(i,j,2)]./norm([vector(i,j,1),vector(i,j,2)]); %% Unit vector
+norm_vec(i,j,:)=[unit_vector(i,j,2),-unit_vector(i,j,1)];%%%  Normal vector, coil direction
+    end
 end
 
 %%%% Create vector of the current from the filaments
@@ -19,22 +21,13 @@ for j=2:7
 end  
 end
 
-%%%%% Vectorial sum from the effect of the filaments on each mirnov coil
-for i=1:12
-Bz_tot(i)=0;
-BR_tot(i)=0;
-end
 
+%%% Lets make the sum as it has to be done
 for i=1:12
+Bmirn(i)=0;
 for j=1:7
-     BR_tot(i)=BR_tot(i)+BR(i,j);
-     Bz_tot(i)=Bz_tot(i)+Bz(i,j);
+    Bmirn(i)=Bmirn(i)+dot([Bz(i,j),BR(i,j)],[norm_vec(i,j,1),norm_vec(i,j,2)]);
 end
-end
-
-%%% Calculate the projection 
-for i=1:12
-Bmirn(i)=dot([Bz_tot(i),BR_tot(i)],norm_vec(i,[1,2]));
 end
 
 Bmirn=0.01*Bmirn;%fator de 0.01 pra ter [T] 
